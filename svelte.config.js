3266e6fe4dd902b14bb0466750d856762965c756
refact0r <34758569+refact0r@users.noreply.github.com>
2022-07-06 19:16:33 -0700

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-auto';
+import md from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	preprocess: [md.mdsvex(mdsvexConfig)],
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
+		prerender: {
+			onError: 'continue'
+		}
+	}
+};
+
+export default config;
