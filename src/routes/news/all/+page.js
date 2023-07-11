c0dd5754983784a0656357933755cfc16b1f6733
yifan <34758569+refact0r@users.noreply.github.com>
2023-07-10 17:11:35 -0700

    format

--- src/routes/news/all/+page.js
+++ src/routes/news/all/+page.js
@@ -1,7 +1,7 @@
 import getPosts from '$lib/js/posts.js'
 
 export async function load() {
-	const modules = import.meta.glob(`/src/content/news/*.{md,svx,svelte.md}`)
+	const modules = import.meta.glob('/src/content/news/*.{md,svx,svelte.md}')
 	const posts = await getPosts(modules, 3)
 	return { posts }
 }
